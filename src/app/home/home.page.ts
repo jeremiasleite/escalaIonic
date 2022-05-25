33beccb340bbd38811289a8338af708ee48416a9
jeremiasleite <jeremias23456@gmail.com>
2022-05-25 08:45:45 -0300

    add fuction work

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,36 +6,69 @@ import * as moment from 'moment';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
 export class HomePage {
-
-  months = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'];
+  months = [
+    'JAN',
+    'FEV',
+    'MAR',
+    'ABR',
+    'MAI',
+    'JUN',
+    'JUL',
+    'AGO',
+    'SET',
+    'OUT',
+    'NOV',
+    'DEZ',
+  ];
   weekdays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
   n = 7;
   firstDayOfWeek = moment().startOf('M').day();
   arrayDays: number[];
-  dateNow;
+  dateNow: moment.Moment;
   monthNow: string;
-  yearNow: string;
+  monthNowNumber: number;
+  yearNow: number;
   today: number;
+  firstDayWorkSA = moment('2022-01-08');
   constructor() {
     this.dateNow = moment();
     this.today = this.isToday(this.dateNow);
     this.monthNow = this.months[this.dateNow.month()];
+    this.monthNowNumber = this.dateNow.month();
     this.yearNow = this.dateNow.year();
     this.arrayDays = this.createCalendar(this.dateNow);
+    //console.log(this.isDayA(this.dateNow));
   }
-  public createCalendar(month) {
+  /*public createCalendar(month: moment.Moment) {
     const nDays = moment(month).daysInMonth();
     const firstDay = moment(month).startOf('M').day();
-    const days = Array.from(Array(nDays).keys()).map((x)=>x+1);
-
+    //const days = Array.from(Array(nDays).keys()).map((x)=>x+1);
+    let days: any[];
+    for(let j = 0, ,)
     for (let n = 0; n < firstDay; n++) {
       days.unshift(null);
     }
     const isdivide = days.length % 7;
     if(isdivide !== 0){
       for(let i = 0 ;i < 7-isdivide; i++){
+        days.push(null);
+      }
+    }
+    return days;
+  }*/
+  public createCalendar(month) {
+    const firstDay = moment(month).startOf('M');
+    const days = Array.apply(null, { length: month.daysInMonth() })
+      .map(Number.call, Number)
+      .map((n) => moment(firstDay).add(n, 'd'));
+
+    for (let n = 0; n < firstDay.weekday(); n++) {
+      days.unshift(null);
+    }
+    const isdivide = days.length % 7;
+    if (isdivide !== 0) {
+      for (let i = 0; i < 7 - isdivide; i++) {
         days.push(null);
       }
     }
@@ -57,11 +90,41 @@ export class HomePage {
     this.arrayDays = this.createCalendar(this.dateNow);
     this.today = this.isToday(this.dateNow);
   }
-  public isToday(day: moment.Moment){
-    if(moment().format('L') === day.format('L')){
+
+  public isToday(day: moment.Moment) {
+    if (moment().format('L') === day.format('L')) {
       return +day.format('D');
-    }else{
+    } else {
       return 0;
+    }
+  }
+
+  public isDayA(day: any) {
+    if(day!==null){
+      const duration = moment.duration(day.diff(this.firstDayWorkSA));
+      const days = Math.floor(duration.asDays());
+      const y = days % 9;
+      if (y < 5) {
+        //manhã
+        return 2;
+      } else if (y === 5) {
+        return 1; //noite
+      } else {
+        //folga
+        return 0;
+      }
+    }else{
+      return null;
+    }
+  }
+  /**
+   * formatDay
+  date: any     */
+  public formatDay(date: any) {
+    if(date!==null){
+      return +date.format('D');
+    }else{
+      return null;
     }
   }
 }
