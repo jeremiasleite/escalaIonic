52b8c13be4cddb3aaf49de4e8b4e76d58ecd2eb0
jeremiasleite <jeremias23456@gmail.com>
2022-05-26 06:40:31 -0300

    add firebase, icons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -45,25 +45,8 @@ export class HomePage {
     this.monthNowNumber = this.dateNow.month();
     this.yearNow = this.dateNow.year();
     this.arrayDays = this.createCalendar(this.dateNow);
-    //console.log(this.isDayA(this.dateNow));
   }
-  /*public createCalendar(month: moment.Moment) {
-    const nDays = moment(month).daysInMonth();
-    const firstDay = moment(month).startOf('M').day();
-    //const days = Array.from(Array(nDays).keys()).map((x)=>x+1);
-    let days: any[];
-    for(let j = 0, ,)
-    for (let n = 0; n < firstDay; n++) {
-      days.unshift(null);
-    }
-    const isdivide = days.length % 7;
-    if(isdivide !== 0){
-      for(let i = 0 ;i < 7-isdivide; i++){
-        days.push(null);
-      }
-    }
-    return days;
-  }*/
+
   public createCalendar(month) {
     const firstDay = moment(month).startOf('M');
     const days = Array.apply(null, { length: month.daysInMonth() })
@@ -91,11 +74,13 @@ export class HomePage {
   }
 
   public previousMonth() {
-    this.dateNow.subtract(1, 'M');
-    this.monthNow = this.months[this.dateNow.month()];
-    this.yearNow = this.dateNow.year();
-    this.arrayDays = this.createCalendar(this.dateNow);
-    this.today = this.isToday(this.dateNow);
+    if(this.dateNow.year()>2022 ){
+      this.dateNow.subtract(1, 'M');
+      this.monthNow = this.months[this.dateNow.month()];
+      this.yearNow = this.dateNow.year();
+      this.arrayDays = this.createCalendar(this.dateNow);
+      this.today = this.isToday(this.dateNow);
+    }
   }
 
   public isToday(day: moment.Moment) {
